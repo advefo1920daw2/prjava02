8bb8324450f4fdae6641513418eb9264caee4795
advefo1920daw2 <advefo1920daw2@gmail.com>
2019-11-21 19:47:44 +0100

    Treballant amb ordres Git i branques:Versió 1.1 del projecte prjava02

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -17,15 +17,16 @@ public class Prjava02 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("versiÃ³ 0.1 del projecte prjava02"); 	
+        System.out.println("versió 0.1 del projecte prjava02"); 	
         try {
-            InetAddress adreÃ§a = InetAddress.getLocalHost();
-            String hostname = adreÃ§a.getHostName();
+            InetAddress adreça = InetAddress.getLocalHost();
+            String hostname = adreça.getHostName();
             System.out.println("hostname="+hostname);
             System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
             System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
             System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
-            System.out.println("VersiÃ³ OS: " + System.getProperty("os.version"));
+            System.out.println("Versió OS: " + System.getProperty("os.version"));
+            System.out.println("Creació d'una branca del projecte prjava02");
         }
         catch (IOException e) {
 		System.out.println("Exception occurred");
